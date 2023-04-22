aaae906f7726087c7862de9322b63c0a5b5d84fc
DunkBlocker3000 <36441422+DunkBlocker3000@users.noreply.github.com>
2023-04-22 16:25:49 +1200

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,10 @@
-const path = require("path");
+const path = require('path');
 
 module.exports = {
-  entry: "./src/index.js",
+  entry: './in.js',
   output: {
-    filename: "main.js",
-    path: path.resolve(__dirname, "dist"),
+    filename: 'main.js',
+    path: path.resolve(__dirname)
   },
-  target: "web",
+  mode: 'development'
 };
