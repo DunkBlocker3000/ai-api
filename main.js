004a4bff6e89199f226603c11ecac58e33f64cdf
DunkBlocker3000 <36441422+DunkBlocker3000@users.noreply.github.com>
2023-04-22 17:24:48 +1200

    Update main.js

--- main.js
+++ main.js
@@ -1,65 +1,25 @@
-const openaiApiKey = process.env.OPENAI_API_KEY;
+document.addEventListener("DOMContentLoaded", () => {
+  const input = document.getElementById("input");
+  const output = document.getElementById("output");
+  const generateButton = document.getElementById("generate");
 
-document.addEventListener('DOMContentLoaded', () => {
-  const form = document.querySelector('form');
-  const scenariosDiv = document.querySelector('#scenarios');
-  form.addEventListener('submit', async (event) => {
-    event.preventDefault();
-    scenariosDiv.innerHTML = 'Loading...';
-
-    const url = document.querySelector('#url').value;
-    const text = document.querySelector('#text').value;
-
-    if (!url && !text) {
-      scenariosDiv.innerHTML = 'Please enter a URL or text.';
+  generateButton.addEventListener("click", async () => {
+    const text = input.value.trim();
+    if (text === "") {
+      output.innerHTML = "Please enter some API documentation!";
       return;
     }
 
-    const content = url ? await fetchUrl(url) : text;
-    const scenarios = await generateScenarios(content);
-
-    scenariosDiv.innerHTML = '';
-    scenarios.forEach((scenario, i) => {
-      const scenarioDiv = document.createElement('div');
-      const header = document.createElement('h2');
-      const stepsList = document.createElement('ol');
-      header.innerText = `Scenario ${i + 1}`;
-      scenario.steps.forEach((step) => {
-        const stepItem = document.createElement('li');
-        stepItem.innerText = step;
-        stepsList.appendChild(stepItem);
-      });
-      scenarioDiv.appendChild(header);
-      scenarioDiv.appendChild(stepsList);
-      scenariosDiv.appendChild(scenarioDiv);
+    const prompt = `Generate test scenarios based on this API documentation:\n\n${text}\n\n---\n\nScenario 1:`;
+    const completions = await openai.complete({
+      engine: "text-davinci-002",
+      prompt,
+      maxTokens: 1024,
+      n: 1,
+      stop: ["Scenario"],
     });
+
+    const scenario = completions.choices[0].text.trim();
+    output.innerHTML = `Generated test scenario:<br/><br/>${scenario}`;
   });
 });
-
-async function fetchUrl(url) {
-  const response = await fetch(url);
-  if (!response.ok) {
-    throw new Error(`Failed to fetch ${url}: ${response.status} ${response.statusText}`);
-  }
-  const text = await response.text();
-  return text;
-}
-
-async function generateScenarios(text) {
-  const openai = new OpenAI(openaiApiKey);
-  const prompt = `Generate test scenarios based on the following:\n${text}\n\nScenario 1:`;
-  const completions = await openai.complete({
-    engine: 'text-davinci-002',
-    prompt,
-    maxTokens: 2048,
-    n: 1,
-    stop: 'Scenario'
-  });
-  const scenarios = [];
-  completions.choices.forEach((choice) => {
-    const text = choice.text.trim();
-    const steps = text.split('\n').slice(1);
-    scenarios.push({ steps });
-  });
-  return scenarios;
-}
