8d40548c2a57f346952f89fb4153f578f4454389
DunkBlocker3000 <36441422+DunkBlocker3000@users.noreply.github.com>
2023-04-22 16:26:09 +1200

    Update main.js

--- main.js
+++ main.js
@@ -1,59 +1,57 @@
-const form = document.getElementById("api-form");
-const resultContainer = document.getElementById("result-container");
+let apiKey;
+let apiUrl;
+let apiUrlPrefix;
 
-form.addEventListener("submit", async (event) => {
-  event.preventDefault();
-  resultContainer.innerHTML = "";
-
-  const url = event.target.url.value;
-
-  try {
-    const response = await fetch(url);
-    const data = await response.json();
-
-    const scenarios = extractScenarios(data);
-    renderScenarios(scenarios);
-  } catch (error) {
-    console.error(error);
-    resultContainer.textContent = "An error occurred while fetching data from the API";
-  }
-});
-
-function extractScenarios(data) {
-  const scenarios = [];
-
-  for (const key in data) {
-    const value = data[key];
-    if (typeof value === "object" && value !== null) {
-      scenarios.push({ name: key, request: value });
-    }
-  }
-
-  return scenarios;
+function setApiKey() {
+  apiKey = document.getElementById('api-key').value;
 }
 
-function renderScenarios(scenarios) {
-  if (scenarios.length === 0) {
-    resultContainer.textContent = "No scenarios found in API response";
-    return;
+function setApiUrl() {
+  apiUrl = document.getElementById('url-input').value;
+  apiUrlPrefix = apiUrl.substring(0, apiUrl.lastIndexOf('/') + 1);
+}
+
+async function generateScenarios() {
+  setApiKey();
+  setApiUrl();
+  
+  const response = await fetch(apiUrl);
+  const text = await response.text();
+  const parser = new DOMParser();
+  const doc = parser.parseFromString(text, 'text/html');
+  const scenarioElements = doc.querySelectorAll('[data-scenario]');
+  const scenarios = [];
+
+  for (let i = 0; i < scenarioElements.length; i++) {
+    const scenario = scenarioElements[i].getAttribute('data-scenario');
+    const url = scenarioElements[i].getAttribute('data-url');
+    const method = scenarioElements[i].getAttribute('data-method');
+    const headers = scenarioElements[i].getAttribute('data-headers');
+    const body = scenarioElements[i].getAttribute('data-body');
+
+    const headersObj = headers ? JSON.parse(headers) : {};
+
+    if (apiKey && !headersObj.Authorization) {
+      headersObj.Authorization = `Bearer ${apiKey}`;
+    }
+
+    const options = {
+      method: method,
+      headers: headersObj,
+      mode: 'cors'
+    };
+
+    if (body) {
+      options.body = body;
+    }
+
+    scenarios.push({
+      scenario: scenario,
+      url: apiUrlPrefix + url,
+      options: options
+    });
   }
 
-  const scenariosList = document.createElement("ul");
-  scenarios.forEach((scenario) => {
-    const listItem = document.createElement("li");
-    const link = document.createElement("a");
-    link.href = "#";
-    link.textContent = scenario.name;
-    link.addEventListener("click", (event) => {
-      event.preventDefault();
-      const result = document.createElement("pre");
-      resultContainer.innerHTML = "";
-      result.textContent = JSON.stringify(scenario.request, null, 2);
-      resultContainer.appendChild(result);
-    });
-    listItem.appendChild(link);
-    scenariosList.appendChild(listItem);
-  });
-
-  resultContainer.appendChild(scenariosList);
+  document.getElementById('scenarios').value = JSON.stringify(scenarios, null, 2);
+  document.getElementById('scenarios').disabled = false;
 }
