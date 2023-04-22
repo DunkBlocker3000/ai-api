e1b4ebe10c5b801d016ee6594ce12a7f80fa7ad6
DunkBlocker3000 <36441422+DunkBlocker3000@users.noreply.github.com>
2023-04-22 15:07:10 +1200

    Update main.js

--- main.js
+++ main.js
@@ -1,45 +1,49 @@
-const apiUrlInput = document.getElementById('apiUrl');
-const apiForm = document.getElementById('apiForm');
-const scenarioList = document.getElementById('scenarioList');
-const selectedScenarios = document.getElementById('selected');
-const selectedForm = document.getElementById('selectedScenarios');
+const generateRequests = async (docUrl, selectedScenarios) => {
+  const docText = await fetchDocText(docUrl);
+  const scenarios = extractScenarios(docText);
+  const requests = scenarios.filter((scenario) => selectedScenarios.includes(scenario.name)).map((scenario) => {
+    const requestBody = generateRequestBody(scenario);
+    const request = generateCurlRequest(scenario.method, scenario.url, requestBody);
+    return request;
+  });
+  return requests;
+};
 
-apiForm.addEventListener('submit', async (e) => {
-    e.preventDefault();
-    const apiUrl = apiUrlInput.value;
-    const scenarioData = await fetch(apiUrl).then(response => response.json());
-    renderScenarios(scenarioData);
-});
+const fetchDocText = async (docUrl) => {
+  const response = await fetch(docUrl);
+  const text = await response.text();
+  return text;
+};
 
-function renderScenarios(scenarios) {
-    scenarioList.innerHTML = '';
-    scenarios.forEach((scenario, index) => {
-        const checkbox = document.createElement('input');
-        checkbox.type = 'checkbox';
-        checkbox.name = `scenario-${index}`;
-        checkbox.value = JSON.stringify(scenario);
-        const label = document.createElement('label');
-        label.htmlFor = checkbox.name;
-        label.textContent = scenario.name;
-        const div = document.createElement('div');
-        div.appendChild(checkbox);
-        div.appendChild(label);
-        scenarioList.appendChild(div);
-    });
-}
+const extractScenarios = (docText) => {
+  // Use ChatGPT to extract scenarios
+  const prompt = `Extract the scenarios from the following API documentation:\n\n${docText}\n\n`;
+  const scenariosText = generateText(prompt);
+  const scenarios = JSON.parse(scenariosText);
+  return scenarios;
+};
 
-selectedForm.addEventListener('submit', (e) => {
-    e.preventDefault();
-    const options = Array.from(selectedScenarios.options);
-    const selected = options.filter(option => option.selected);
-    const requests = selected.map(option => JSON.parse(option.value));
-    generateRequests(requests);
-});
+const generateRequestBody = (scenario) => {
+  // Use ChatGPT to generate request body
+  const prompt = `Generate the request body for the following scenario:\n\n${JSON.stringify(scenario, null, 2)}\n\n`;
+  const requestBody = generateText(prompt);
+  return requestBody;
+};
 
-function generateRequests(requests) {
-    requests.forEach(request => {
-        const { method, url, body } = request;
-        const curlCommand = `curl -X ${method} ${url} -d '${JSON.stringify(body)}'`;
-        console.log(curlCommand);
-    });
-}
+const generateCurlRequest = (method, url, requestBody) => {
+  let curlRequest = `curl -X ${method} ${url}`;
+  if (requestBody) {
+    curlRequest += ` -d '${requestBody}'`;
+  }
+  return curlRequest;
+};
+
+const generateText = async (prompt) => {
+  // Use OpenAI API to generate text
+  const response = await fetch('https://api.openai.com/v1/engines/davinci-codex/completions', {
+    method: 'POST',
+headers: {
+    'Content-Type': 'application/json',
+    'Authorization': 'Bearer ' + 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
+},
+    body:
