6b84417026db40b65448d9ee41705701fabaa548
DunkBlocker3000 <36441422+DunkBlocker3000@users.noreply.github.com>
2023-04-22 15:57:32 +1200

    Update main.js

--- main.js
+++ main.js
@@ -1,45 +1,69 @@
-const generateScenariosButton = document.getElementById("generate-scenarios-button");
-const scenarioList = document.getElementById("scenario-list");
+const endpointList = ["davinci", "curie", "babbage", "ada"];
 
-async function analyzeDocumentation() {
-  const response = await fetch("https://api.openai.com/v1/engines/davinci-codex/completions", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-      "Authorization": `Bearer ${process.env.OPENAI_API_KEY}`,
-    },
-    body: JSON.stringify({
-      prompt: "Python function to get a list of unique values from a list of dictionaries\n\n```python\nlist_of_dicts = [{'name': 'Alice', 'age': 25}, {'name': 'Bob', 'age': 30}, {'name': 'Alice', 'age': 20}, {'name': 'Charlie', 'age': 25}]\n```",
-      max_tokens: 60,
-      temperature: 0.7,
-      n: 1,
-      stop: "\n```",
-    }),
-  });
-  const data = await response.json();
-  const code = data.choices[0].text;
-  return code.trim();
+function generateScenarios() {
+  const model = document.getElementById("model").value;
+  const prompt = document.getElementById("prompt").value;
+  const length = document.getElementById("length").value;
+  const apiKey = process.env.OPENAI_API_KEY;
+  analyzeDocumentation(model, apiKey)
+    .then((result) => {
+      const docs = result.data.data;
+      const scenarioList = generateScenarioList(docs, prompt, length);
+      renderScenarioList(scenarioList);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
 }
 
-async function generateScenarioList() {
-  const code = await analyzeDocumentation();
-  const endpointList = ["Create User", "Get User", "Update User", "Delete User"];
-  const regex = /\n\s*# Function to (.+?)\n\s*def \w+\(/g;
-  const matches = Array.from(code.matchAll(regex), (m) => m[1]);
-  const scenarioList = matches.filter((match) => endpointList.includes(match));
+function analyzeDocumentation(model, apiKey) {
+  const url = `https://api.openai.com/v1/models/${model}/documentation`;
+  const headers = {
+    "Content-Type": "application/json",
+    Authorization: `Bearer ${apiKey}`,
+  };
+  const options = {
+    headers,
+    method: "GET",
+  };
+  return fetch(url, options).then((response) => response.json());
+}
+
+function generateScenarioList(docs, prompt, length) {
+  const scenarioList = [];
+  for (const endpoint of endpointList) {
+    for (const doc of docs) {
+      const promptWithDoc = prompt.replace("[DOCUMENTATION]", doc);
+      const promptWithDocAndEndpoint = promptWithDoc.replace(
+        "[ENDPOINT]",
+        endpoint
+      );
+      scenarioList.push({
+        model: doc,
+        endpoint,
+        prompt: promptWithDocAndEndpoint,
+        length,
+      });
+    }
+  }
   return scenarioList;
 }
 
-async function generateScenarios() {
-  generateScenariosButton.disabled = true;
-  scenarioList.innerHTML = "";
-  const scenarioListItems = await generateScenarioList();
-  scenarioListItems.forEach((scenario) => {
-    const li = document.createElement("li");
-    li.textContent = scenario;
-    scenarioList.appendChild(li);
-  });
-  generateScenariosButton.disabled = false;
+function renderScenarioList(scenarioList) {
+  const container = document.getElementById("scenarios");
+  container.innerHTML = "";
+  for (const scenario of scenarioList) {
+    const scenarioElement = document.createElement("div");
+    scenarioElement.className = "scenario";
+    scenarioElement.innerHTML = `
+      <h3>Scenario:</h3>
+      <ul>
+        <li><strong>Model:</strong> ${scenario.model}</li>
+        <li><strong>Endpoint:</strong> ${scenario.endpoint}</li>
+        <li><strong>Prompt:</strong> ${scenario.prompt}</li>
+        <li><strong>Length:</strong> ${scenario.length}</li>
+      </ul>
+    `;
+    container.appendChild(scenarioElement);
+  }
 }
-
-generateScenariosButton.onclick = generateScenarios;
