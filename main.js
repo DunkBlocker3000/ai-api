d8e9a9798bfd50b6b1443320e7ac97572ecad8d6
DunkBlocker3000 <36441422+DunkBlocker3000@users.noreply.github.com>
2023-04-22 16:29:22 +1200

    Update main.js

--- main.js
+++ main.js
@@ -1,57 +1,28 @@
-let apiKey;
-let apiUrl;
-let apiUrlPrefix;
+const apiUrlInput = document.getElementById('apiUrl');
+const generateBtn = document.getElementById('generateBtn');
+const loadingMsg = document.getElementById('loadingMsg');
+const scenariosList = document.getElementById('scenarios');
 
-function setApiKey() {
-  apiKey = document.getElementById('api-key').value;
-}
-
-function setApiUrl() {
-  apiUrl = document.getElementById('url-input').value;
-  apiUrlPrefix = apiUrl.substring(0, apiUrl.lastIndexOf('/') + 1);
-}
-
-async function generateScenarios() {
-  setApiKey();
-  setApiUrl();
-  
-  const response = await fetch(apiUrl);
-  const text = await response.text();
-  const parser = new DOMParser();
-  const doc = parser.parseFromString(text, 'text/html');
-  const scenarioElements = doc.querySelectorAll('[data-scenario]');
-  const scenarios = [];
-
-  for (let i = 0; i < scenarioElements.length; i++) {
-    const scenario = scenarioElements[i].getAttribute('data-scenario');
-    const url = scenarioElements[i].getAttribute('data-url');
-    const method = scenarioElements[i].getAttribute('data-method');
-    const headers = scenarioElements[i].getAttribute('data-headers');
-    const body = scenarioElements[i].getAttribute('data-body');
-
-    const headersObj = headers ? JSON.parse(headers) : {};
-
-    if (apiKey && !headersObj.Authorization) {
-      headersObj.Authorization = `Bearer ${apiKey}`;
-    }
-
-    const options = {
-      method: method,
-      headers: headersObj,
-      mode: 'cors'
-    };
-
-    if (body) {
-      options.body = body;
-    }
-
-    scenarios.push({
-      scenario: scenario,
-      url: apiUrlPrefix + url,
-      options: options
+generateBtn.addEventListener('click', () => {
+  loadingMsg.textContent = 'Loading...';
+  scenariosList.innerHTML = '';
+  generateScenarios(apiUrlInput.value)
+    .then(scenarios => {
+      loadingMsg.textContent = '';
+      for (const scenario of scenarios) {
+        const li = document.createElement('li');
+        li.textContent = scenario;
+        scenariosList.appendChild(li);
+      }
+    })
+    .catch(error => {
+      loadingMsg.textContent = '';
+      console.error(error);
     });
-  }
+});
 
-  document.getElementById('scenarios').value = JSON.stringify(scenarios, null, 2);
-  document.getElementById('scenarios').disabled = false;
+async function generateScenarios(apiUrl) {
+  const api = await openApi.load(apiUrl);
+  const generator = new ScenarioGenerator(api);
+  return generator.generate();
 }
