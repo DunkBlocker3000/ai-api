80bfae90ce8ed6593f5beec0ebf5e2acc9d75c63
DunkBlocker3000 <36441422+DunkBlocker3000@users.noreply.github.com>
2023-04-22 18:32:49 +1200

    Update main.js

--- main.js
+++ main.js
@@ -1,57 +1,73 @@
-const generateButton = document.querySelector('#generate-button');
-const textArea = document.querySelector('#text-area');
-const scenarioList = document.querySelector('#scenario-list');
+const generateButton = document.getElementById("generate-button");
+const createTestsButton = document.getElementById("create-tests-button");
+const scenarioList = document.getElementById("scenario-list");
+const inputBox = document.getElementById("input");
 
-generateButton.addEventListener('click', async () => {
-  const text = textArea.value;
-  if (text.trim() === '') {
-    alert('Please enter some text');
-    return;
+const prompt = "Provide a comprehensive list of scenarios to test the software described in this text.";
+
+const openaiApiKey = process.env.OPENAI_API_KEY; // Make sure to set this as an environment variable
+
+generateButton.addEventListener("click", async () => {
+  try {
+    const scenarios = await generateScenarios(inputBox.value);
+    if (scenarios.length === 0) {
+      alert("No scenarios generated!");
+      return;
+    }
+    scenarioList.innerHTML = "";
+    scenarios.forEach((scenario) => {
+      const scenarioDiv = document.createElement("div");
+      const radio = document.createElement("input");
+      radio.type = "radio";
+      radio.name = "scenario";
+      radio.value = scenario;
+      scenarioDiv.appendChild(radio);
+      const label = document.createElement("label");
+      label.innerHTML = scenario;
+      scenarioDiv.appendChild(label);
+      scenarioList.appendChild(scenarioDiv);
+    });
+    createTestsButton.style.display = "block";
+  } catch (err) {
+    console.error(err);
+    alert("Failed to generate scenarios: " + err.message);
   }
-
-  const response = await fetch(`https://api.openai.com/v1/engines/davinci-codex/completions`, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-      'Authorization': `Bearer ${process.env.OPENAI_API_KEY}`
-    },
-    body: JSON.stringify({
-      prompt: `Provide a comprehensive list of scenarios to test the software described in this text:\n\n${text}`,
-      max_tokens: 60,
-      n: 1,
-      stop: ['\n\n']
-    })
-  });
-
-  if (!response.ok) {
-    throw new Error(`Failed to generate scenarios: ${response.status}`);
-  }
-
-  const data = await response.json();
-
-  const scenarios = data.choices[0].text.trim().split('\n');
-
-  if (scenarios.length === 0) {
-    alert('No scenarios were generated');
-    return;
-  }
-
-  scenarioList.innerHTML = '';
-  scenarios.forEach((scenario, index) => {
-    const radioInput = document.createElement('input');
-    radioInput.type = 'radio';
-    radioInput.name = 'scenario';
-    radioInput.id = `scenario-${index}`;
-    radioInput.value = scenario;
-
-    const radioLabel = document.createElement('label');
-    radioLabel.setAttribute('for', `scenario-${index}`);
-    radioLabel.innerText = scenario;
-
-    const listItem = document.createElement('li');
-    listItem.appendChild(radioInput);
-    listItem.appendChild(radioLabel);
-
-    scenarioList.appendChild(listItem);
-  });
 });
+
+async function generateScenarios(text) {
+  if (!openaiApiKey) {
+    throw new Error("OpenAI API key not set");
+  }
+  const openai = new OpenAI(openaiApiKey);
+  const completions = await openai.complete({
+    prompt: prompt,
+    max_tokens: 200,
+    n: 1,
+    stop: ["\n\n"],
+    temperature: 0.5,
+    engine: "text-davinci-002",
+    prompt_context: text,
+  });
+  if (!completions.choices || completions.choices.length === 0) {
+    throw new Error("Failed to generate scenarios");
+  }
+  return completions.choices[0].text.trim().split("\n");
+}
+
+createTestsButton.addEventListener("click", () => {
+  const selectedScenario = document.querySelector(
+    'input[name="scenario"]:checked'
+  );
+  if (!selectedScenario) {
+    alert("Please select a scenario!");
+    return;
+  }
+  const text = selectedScenario.value;
+  const filename = text.toLowerCase().replace(/[^a-z0-9]+/g, "-");
+  if (/api/i.test(text)) {
+    // create api file
+    const content = `// TODO: Implement tests for ${text}`;
+    downloadFile(`${filename}.js`, content);
+  } else {
+    // create csv file
+    const content = `Scenario,
