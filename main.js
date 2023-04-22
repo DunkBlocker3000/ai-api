81e9e95b9311c58df47b1982d9559d1e0e5f47ae
DunkBlocker3000 <36441422+DunkBlocker3000@users.noreply.github.com>
2023-04-22 16:32:17 +1200

    Update main.js

--- main.js
+++ main.js
@@ -1,28 +1,48 @@
-const apiUrlInput = document.getElementById('apiUrl');
-const generateBtn = document.getElementById('generateBtn');
-const loadingMsg = document.getElementById('loadingMsg');
-const scenariosList = document.getElementById('scenarios');
+const form = document.querySelector("form");
+const urlInput = document.querySelector("#url-input");
+const scenarios = document.querySelector("#scenarios");
+const note = document.querySelector("#note");
 
-generateBtn.addEventListener('click', () => {
-  loadingMsg.textContent = 'Loading...';
-  scenariosList.innerHTML = '';
-  generateScenarios(apiUrlInput.value)
-    .then(scenarios => {
-      loadingMsg.textContent = '';
-      for (const scenario of scenarios) {
-        const li = document.createElement('li');
-        li.textContent = scenario;
-        scenariosList.appendChild(li);
-      }
-    })
-    .catch(error => {
-      loadingMsg.textContent = '';
-      console.error(error);
-    });
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  generateScenarios(urlInput.value);
 });
 
-async function generateScenarios(apiUrl) {
-  const api = await openApi.load(apiUrl);
-  const generator = new ScenarioGenerator(api);
-  return generator.generate();
+async function generateScenarios(url) {
+  note.innerText = "Generating scenarios...";
+  const response = await fetch(url);
+  const text = await response.text();
+  const parser = new DOMParser();
+  const htmlDoc = parser.parseFromString(text, "text/html");
+  const scenariosArray = [];
+
+  // Get all API endpoints
+  const endpoints = Array.from(htmlDoc.querySelectorAll("a[href]")).filter(
+    (link) =>
+      link.getAttribute("href").startsWith("/") ||
+      link.getAttribute("href").startsWith("http")
+  );
+
+  if (endpoints.length === 0) {
+    scenarios.innerHTML = "";
+    note.innerText = "No endpoints found on the given API documentation page.";
+    return;
+  }
+
+  endpoints.forEach((endpoint) => {
+    const method = endpoint.closest("tr").querySelector(".method").innerText;
+    const href = endpoint.getAttribute("href");
+    const description = endpoint.closest("tr").querySelector(".desc").innerText;
+    const scenario = `${method} ${href}: ${description}`;
+    scenariosArray.push(scenario);
+  });
+
+  scenarios.innerHTML = "";
+  scenariosArray.forEach((scenario) => {
+    const li = document.createElement("li");
+    li.innerText = scenario;
+    scenarios.appendChild(li);
+  });
+
+  note.innerText = `Generated ${scenariosArray.length} scenarios from ${endpoints.length} API endpoints.`;
 }
