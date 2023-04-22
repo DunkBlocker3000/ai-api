d3a7894b457f0b71c2f019c831bd9cd913293005
DunkBlocker3000 <36441422+DunkBlocker3000@users.noreply.github.com>
2023-04-22 17:13:26 +1200

    Update main.js

--- main.js
+++ main.js
@@ -1,80 +1,41 @@
-// get input elements
-const urlInput = document.getElementById('url-input');
-const textInput = document.getElementById('text-input');
-const generateBtn = document.getElementById('generate-btn');
+window.addEventListener('DOMContentLoaded', () => {
+  const urlInput = document.getElementById('urlInput');
+  const textarea = document.getElementById('textarea');
+  const generateBtn = document.getElementById('generateBtn');
+  const output = document.getElementById('output');
 
-// handle generate button click
-generateBtn.addEventListener('click', async () => {
-  // get API documentation
-  const apiDoc = urlInput.value
-    ? await fetchApiDoc(urlInput.value)
-    : textInput.value.trim();
-
-  // generate test scenarios
-  const testScenarios = await generateTestScenarios(apiDoc);
-
-  // display test scenarios
-  displayTestScenarios(testScenarios);
-});
-
-// function to fetch API documentation from URL
-async function fetchApiDoc(url) {
-  try {
-    const response = await fetch(url);
-    if (!response.ok) {
-      throw new Error('Unable to fetch API documentation');
+  generateBtn.addEventListener('click', async () => {
+    output.innerText = 'Generating scenarios...';
+    let input;
+    if (urlInput.value) {
+      const response = await fetch(urlInput.value);
+      input = await response.text();
+    } else if (textarea.value) {
+      input = textarea.value;
+    } else {
+      output.innerText = 'Please provide a URL or enter API documentation in the text box.';
+      return;
     }
-    const data = await response.text();
-    return data;
-  } catch (err) {
-    console.error(err);
-  }
-}
 
-// function to generate test scenarios using OpenAI
-async function generateTestScenarios(apiDoc) {
-  try {
+    const data = {
+      model: 'text-davinci-002',
+      prompt: `Generate test scenarios for the following API documentation:\n${input}`,
+      temperature: 0.5,
+      max_tokens: 60,
+      n: 1,
+      stop: ['\n\n']
+    };
+    
     const response = await fetch('https://api.openai.com/v1/engines/davinci-codex/completions', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
         'Authorization': `Bearer ${process.env.OPENAI_API_KEY}`
       },
-      body: JSON.stringify({
-        prompt: `Generate test scenarios for API documentation:\n\n${apiDoc}\n\n`,
-        temperature: 0.7,
-        max_tokens: 1024,
-        top_p: 1,
-        frequency_penalty: 0,
-        presence_penalty: 0
-      })
+      body: JSON.stringify(data)
     });
-    const { choices } = await response.json();
-    const testScenarios = choices.map(choice => choice.text.trim());
-    return testScenarios;
-  } catch (err) {
-    console.error(err);
-  }
-}
-
-// function to display test scenarios
-function displayTestScenarios(testScenarios) {
-  const outputElement = document.getElementById('output');
-  outputElement.innerHTML = '';
-  if (testScenarios.length > 0) {
-    const heading = document.createElement('h2');
-    heading.textContent = 'Generated test scenarios:';
-    outputElement.appendChild(heading);
-    const list = document.createElement('ul');
-    testScenarios.forEach(testScenario => {
-      const listItem = document.createElement('li');
-      listItem.textContent = testScenario;
-      list.appendChild(listItem);
-    });
-    outputElement.appendChild(list);
-  } else {
-    const message = document.createElement('p');
-    message.textContent = 'No test scenarios generated.';
-    outputElement.appendChild(message);
-  }
-}
+    
+    const result = await response.json();
+    output.innerText = result.choices[0].text;
+  });
+});
