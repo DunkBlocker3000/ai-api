7ed482b01ddd7056fb999ab59cab76f50a0e831c
DunkBlocker3000 <36441422+DunkBlocker3000@users.noreply.github.com>
2023-04-22 15:56:12 +1200

    Update main.js

--- main.js
+++ main.js
@@ -1,71 +1,45 @@
-function generateScenarios() {
-  const docUrl = document.getElementById('docUrl').value;
-  const endpointList = analyzeDocumentation(docUrl);
-  endpointList.then(function(result) {
-    const scenarioList = generateScenarioList(result);
-    const scenarioListElem = document.getElementById('scenarioList');
-    scenarioListElem.innerHTML = '';
-    for (const scenario of scenarioList) {
-      const scenarioElem = document.createElement('p');
-      scenarioElem.innerText = scenario;
-      scenarioListElem.appendChild(scenarioElem);
-    }
+const generateScenariosButton = document.getElementById("generate-scenarios-button");
+const scenarioList = document.getElementById("scenario-list");
+
+async function analyzeDocumentation() {
+  const response = await fetch("https://api.openai.com/v1/engines/davinci-codex/completions", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Authorization": `Bearer ${process.env.OPENAI_API_KEY}`,
+    },
+    body: JSON.stringify({
+      prompt: "Python function to get a list of unique values from a list of dictionaries\n\n```python\nlist_of_dicts = [{'name': 'Alice', 'age': 25}, {'name': 'Bob', 'age': 30}, {'name': 'Alice', 'age': 20}, {'name': 'Charlie', 'age': 25}]\n```",
+      max_tokens: 60,
+      temperature: 0.7,
+      n: 1,
+      stop: "\n```",
+    }),
   });
+  const data = await response.json();
+  const code = data.choices[0].text;
+  return code.trim();
 }
 
-async function analyzeDocumentation(docUrl) {
-  // Call OpenAI API to analyze documentation
-  const prompt = `Please analyze the documentation at this URL: ${docUrl}`;
-  const result = await getOpenAIResponse(prompt);
-  const endpointList = JSON.parse(result.choices[0].text);
-  return endpointList;
-}
-
-function generateScenarioList(endpointList) {
-  const scenarioList = [];
-  for (const endpoint of endpointList) {
-    const scenario = generateScenario(endpoint);
-    scenarioList.push(scenario);
-  }
+async function generateScenarioList() {
+  const code = await analyzeDocumentation();
+  const endpointList = ["Create User", "Get User", "Update User", "Delete User"];
+  const regex = /\n\s*# Function to (.+?)\n\s*def \w+\(/g;
+  const matches = Array.from(code.matchAll(regex), (m) => m[1]);
+  const scenarioList = matches.filter((match) => endpointList.includes(match));
   return scenarioList;
 }
 
-function generateScenario(endpoint) {
-  const method = endpoint.method.toUpperCase();
-  const path = endpoint.path;
-  const requiredParams = endpoint.required_params;
-  const optionalParams = endpoint.optional_params;
-  const scenario = `${method} ${path}\n`;
-  if (requiredParams.length > 0) {
-    scenario += 'Required Parameters:\n';
-    for (const param of requiredParams) {
-      scenario += `- ${param}\n`;
-    }
-  }
-  if (optionalParams.length > 0) {
-    scenario += 'Optional Parameters:\n';
-    for (const param of optionalParams) {
-      scenario += `- ${param}\n`;
-    }
-  }
-  return scenario;
+async function generateScenarios() {
+  generateScenariosButton.disabled = true;
+  scenarioList.innerHTML = "";
+  const scenarioListItems = await generateScenarioList();
+  scenarioListItems.forEach((scenario) => {
+    const li = document.createElement("li");
+    li.textContent = scenario;
+    scenarioList.appendChild(li);
+  });
+  generateScenariosButton.disabled = false;
 }
 
-async function getOpenAIResponse(prompt) {
-  const response = await fetch('https://api.openai.com/v1/engines/davinci-codex/completions', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-      'Authorization': `Bearer ${process.env.OPENAI_API_KEY}`
-    },
-    body: JSON.stringify({
-      'prompt': prompt,
-      'temperature': 0.7,
-      'max_tokens': 60,
-      'n': 1,
-      'stop': '\n'
-    })
-  });
-  const data = await response.json();
-  return data;
-}
+generateScenariosButton.onclick = generateScenarios;
