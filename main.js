183d2232d76b8a0991202c41d3ae3b2c3ac9080a
DunkBlocker3000 <36441422+DunkBlocker3000@users.noreply.github.com>
2023-04-22 17:06:03 +1200

    Update main.js

--- main.js
+++ main.js
@@ -1,40 +1,35 @@
-const generateScenarios = async () => {
-  const input = document.getElementById("api-data").value;
-  const apiKey = process.env.OPENAI_API_KEY; // reference OpenAI API Key environment variable
-  const prompt = `Given the following API documentation, generate 10 sample test scenarios:\n\n${input}\n\nSample scenario 1:`;
-  const requestBody = {
-    prompt,
-    temperature: 0.7,
-    max_tokens: 100,
-    n: 10,
-    stop: "\n\nSample scenario",
-  };
-  const response = await fetch("https://api.openai.com/v1/engines/davinci-codex/completions", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-      "Authorization": `Bearer ${apiKey}`, // use OpenAI API Key environment variable
-    },
-    body: JSON.stringify(requestBody),
-  });
-  const responseBody = await response.json();
-  const scenarios = responseBody.choices[0].text
-    .split("Sample scenario")
-    .slice(1)
-    .map((scenario) => scenario.trim());
-  return scenarios;
-};
+document.addEventListener('DOMContentLoaded', () => {
+  const generateBtn = document.querySelector('#generate-btn');
+  const inputUrl = document.querySelector('#input-url');
+  const inputText = document.querySelector('#input-text');
+  const output = document.querySelector('#output');
+  
+  generateBtn.addEventListener('click', () => {
+    const url = inputUrl.value.trim();
+    const text = inputText.value.trim();
 
-const renderScenarios = async () => {
-  const scenarios = await generateScenarios();
-  const scenariosContainer = document.getElementById("scenarios");
-  scenariosContainer.innerHTML = "";
-  scenarios.forEach((scenario) => {
-    const scenarioEl = document.createElement("div");
-    scenarioEl.innerText = scenario;
-    scenariosContainer.appendChild(scenarioEl);
-  });
-};
+    if (url === '' && text === '') {
+      output.innerHTML = 'Please enter a URL or paste API documentation text.';
+      return;
+    }
 
-const submitButton = document.getElementById("submit-button");
-submitButton.addEventListener("click", renderScenarios);
+    const data = { url, text };
+    fetch('/generate', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(data)
+    })
+      .then(response => response.json())
+      .then(data => {
+        if (data.error) {
+          output.innerHTML = `Error: ${data.error}`;
+        } else {
+          output.innerHTML = data.output;
+        }
+      })
+      .catch(error => {
+        console.error(error);
+        output.innerHTML = 'An error occurred. Please try again later.';
+      });
+  });
+});
