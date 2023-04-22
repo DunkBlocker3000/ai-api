dd54bc303016f9af7c38ed5dff9447d67fd78b50
DunkBlocker3000 <36441422+DunkBlocker3000@users.noreply.github.com>
2023-04-22 18:21:19 +1200

    Update main.js

--- main.js
+++ main.js
@@ -1,78 +1,69 @@
-const api = window.openai;
-const generateScenariosBtn = document.getElementById('generate-scenarios-btn');
-const createTestsBtn = document.getElementById('create-tests-btn');
+const generateButton = document.getElementById("generate-button");
+const createTestsButton = document.getElementById("create-tests-button");
+const scenariosDiv = document.getElementById("scenarios");
 
-generateScenariosBtn.addEventListener('click', async () => {
-  const inputText = document.getElementById('input-text').value.trim();
-  if (!inputText) {
-    alert('Please enter some text');
+generateButton.addEventListener("click", async () => {
+  scenariosDiv.innerHTML = "";
+  createTestsButton.disabled = true;
+
+  const text = document.getElementById("text").value;
+  const response = await fetch("/generate-scenarios", {
+    method: "POST",
+    body: JSON.stringify({ text }),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+
+  if (!response.ok) {
+    throw new Error(`Failed to generate scenarios: ${response.status} ${response.statusText}`);
+  }
+
+  const { scenarios } = await response.json();
+
+  scenarios.forEach((scenario) => {
+    const radio = document.createElement("input");
+    radio.type = "radio";
+    radio.name = "scenario";
+    radio.value = scenario;
+
+    const label = document.createElement("label");
+    label.textContent = scenario;
+
+    const br = document.createElement("br");
+
+    scenariosDiv.appendChild(radio);
+    scenariosDiv.appendChild(label);
+    scenariosDiv.appendChild(br);
+  });
+
+  createTestsButton.disabled = false;
+});
+
+createTestsButton.addEventListener("click", async () => {
+  const selectedScenario = document.querySelector('input[name="scenario"]:checked');
+
+  if (!selectedScenario) {
+    alert("Please select a scenario.");
     return;
   }
 
-  generateScenariosBtn.disabled = true;
-  generateScenariosBtn.textContent = 'Generating...';
-
-  const prompt = "Provide a comprehensive list of scenarios to test the software described in this text.";
-  const completions = await api.Completion.create({
-    engine: 'davinci',
-    prompt: prompt,
-    maxTokens: 60,
-    n: 3,
-    stop: '\n\n',
-    temperature: 0.5,
-    promptOverrides: {
-      prompt: prompt + '\n' + inputText
-    }
+  const response = await fetch("/create-tests", {
+    method: "POST",
+    body: JSON.stringify({ scenario: selectedScenario.value }),
+    headers: {
+      "Content-Type": "application/json",
+    },
   });
-  
-  const scenarioList = document.createElement('ul');
-  completions.choices.forEach((choice, index) => {
-    const scenario = document.createElement('li');
-    const radio = document.createElement('input');
-    radio.type = 'radio';
-    radio.name = 'scenario';
-    radio.value = index;
-    if (index === 0) {
-      radio.checked = true;
-    }
-    const label = document.createElement('label');
-    label.textContent = choice.text.trim();
-    label.htmlFor = `scenario-${index}`;
 
-    scenario.appendChild(radio);
-    scenario.appendChild(label);
-    scenarioList.appendChild(scenario);
-  });
-  
-  createTestsBtn.style.display = 'block';
-  document.getElementById('scenarios').innerHTML = '';
-  document.getElementById('scenarios').appendChild(scenarioList);
-  generateScenariosBtn.disabled = false;
-  generateScenariosBtn.textContent = 'Generate Scenarios';
+  if (!response.ok) {
+    throw new Error(`Failed to create tests: ${response.status} ${response.statusText}`);
+  }
+
+  const { downloadUrl, type } = await response.json();
+
+  const link = document.createElement("a");
+  link.href = downloadUrl;
+  link.download = `tests.${type}`;
+  link.click();
 });
-
-createTestsBtn.addEventListener('click', async () => {
-  const selectedRadio = document.querySelector('input[name="scenario"]:checked');
-  const selectedScenario = selectedRadio.value;
-  const scenarioText = completions.choices[selectedScenario].text.trim();
-  
-  createTestsBtn.disabled = true;
-  createTestsBtn.textContent = 'Creating Tests...';
-
-  const filename = 'scenarios.zip';
-  const contentType = 'application/zip';
-  
-  const { generateTests, hasAPI } = generateTest(scenarioText);
-  
-  if (hasAPI) {
-    const zip = new JSZip();
-    const apiFolder = zip.folder('API');
-    generateTests.forEach((test, index) => {
-      apiFolder.file(`api-${index}.json`, JSON.stringify(test, null, 2));
-    });
-    zip.generateAsync({ type: 'blob' }).then((content) => {
-      saveAs(content, filename);
-    });
-  } else {
-    const csvData = generateCSV(generateTests);
-    const blob = new Blob([csvData],
