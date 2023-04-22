7b139f73503c326e7e9bf03f94efd1e439c2c6f6
DunkBlocker3000 <36441422+DunkBlocker3000@users.noreply.github.com>
2023-04-22 17:04:19 +1200

    Update main.js

--- main.js
+++ main.js
@@ -1,38 +1,40 @@
-const inputElement = document.getElementById("input");
-const generateBtn = document.getElementById("generateBtn");
-const outputElement = document.getElementById("output");
-
-generateBtn.addEventListener("click", async () => {
-  outputElement.innerHTML = "Generating scenarios...";
-  const input = inputElement.value.trim();
-  if (input.startsWith("http")) {
-    try {
-      const response = await fetch(input);
-      const text = await response.text();
-      const scenarios = await generateScenariosFromApiDoc(text);
-      outputElement.innerHTML = scenarios;
-    } catch (error) {
-      outputElement.innerHTML = `Error generating scenarios: ${error.message}`;
-    }
-  } else {
-    const scenarios = await generateScenariosFromApiDoc(input);
-    outputElement.innerHTML = scenarios;
-  }
-});
-
-async function generateScenariosFromApiDoc(apiDoc) {
-  // replace any line breaks or tabs with spaces to help with tokenization
-  const cleanApiDoc = apiDoc.replace(/[\n\t]/g, " ");
-  
-  // generate scenarios using OpenAI's GPT-3 API
-  const completions = await openai.complete({
-    engine: "text-davinci-002",
-    prompt: `Generate test scenarios from the following API documentation:\n\n${cleanApiDoc}\n\nScenario 1:`,
-    maxTokens: 1024,
-    n: 1,
-    stop: "Scenario"
+const generateScenarios = async () => {
+  const input = document.getElementById("api-data").value;
+  const apiKey = process.env.OPENAI_API_KEY; // reference OpenAI API Key environment variable
+  const prompt = `Given the following API documentation, generate 10 sample test scenarios:\n\n${input}\n\nSample scenario 1:`;
+  const requestBody = {
+    prompt,
+    temperature: 0.7,
+    max_tokens: 100,
+    n: 10,
+    stop: "\n\nSample scenario",
+  };
+  const response = await fetch("https://api.openai.com/v1/engines/davinci-codex/completions", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Authorization": `Bearer ${apiKey}`, // use OpenAI API Key environment variable
+    },
+    body: JSON.stringify(requestBody),
   });
-  
-  const scenarios = completions.choices[0].text.trim();
+  const responseBody = await response.json();
+  const scenarios = responseBody.choices[0].text
+    .split("Sample scenario")
+    .slice(1)
+    .map((scenario) => scenario.trim());
   return scenarios;
-}
+};
+
+const renderScenarios = async () => {
+  const scenarios = await generateScenarios();
+  const scenariosContainer = document.getElementById("scenarios");
+  scenariosContainer.innerHTML = "";
+  scenarios.forEach((scenario) => {
+    const scenarioEl = document.createElement("div");
+    scenarioEl.innerText = scenario;
+    scenariosContainer.appendChild(scenarioEl);
+  });
+};
+
+const submitButton = document.getElementById("submit-button");
+submitButton.addEventListener("click", renderScenarios);
