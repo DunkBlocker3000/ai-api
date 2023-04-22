86af53ccd1c7f76aeb89a0edaf9a51dd691d3275
DunkBlocker3000 <36441422+DunkBlocker3000@users.noreply.github.com>
2023-04-22 17:32:08 +1200

    Update main.js

--- main.js
+++ main.js
@@ -1,36 +1,29 @@
-document.addEventListener("DOMContentLoaded", function () {
+document.addEventListener('DOMContentLoaded', function() {
   const openaiApiKey = process.env.OPENAI_API_KEY;
-  const generateBtn = document.getElementById("generate-btn");
-  const inputBox = document.getElementById("input");
-  const outputDiv = document.getElementById("output");
-
-  generateBtn.addEventListener("click", function () {
-    const inputText = inputBox.value.trim();
-
-    if (inputText !== "") {
-      generateScenarios(inputText, openaiApiKey);
-    }
-  });
-
-  async function generateScenarios(inputText, apiKey) {
+  const outputContainer = document.getElementById('output-container');
+  const generateButton = document.getElementById('generate-button');
+  
+  generateButton.addEventListener('click', async () => {
+    const inputText = document.getElementById('input-text').value;
+    const inputUrl = document.getElementById('input-url').value;
+    const prompt = `Scenario: ${inputText}\nSource: ${inputUrl}`;
+    
     try {
       const completions = await openai.complete({
-        engine: "text-davinci-002",
-        prompt:
-          "Generate possible scenarios based on the following text:\n\n" +
-          inputText +
-          "\n\n1.",
+        engine: 'davinci',
+        prompt: prompt,
         maxTokens: 60,
-        n: 5,
-        stop: ["2."],
-      }, apiKey);
+        n: 1,
+        stop: '\n',
+        temperature: 0.5,
+      });
 
-      const scenarios = completions.choices.map((choice) => choice.text.trim());
-
-      outputDiv.innerHTML = scenarios.map((scenario, i) => `${i + 1}. ${scenario}<br />`).join("");
-    } catch (error) {
-      console.error(error);
-      outputDiv.innerHTML = "Error generating scenarios.";
+      const scenarios = completions.choices.map(choice => choice.text.trim());
+      const outputHtml = scenarios.map(scenario => `<p>${scenario}</p>`).join('');
+      outputContainer.innerHTML = outputHtml;
+    } catch (err) {
+      console.log(err);
+      outputContainer.innerHTML = '<p>Error generating scenarios. Please try again.</p>';
     }
-  }
+  });
 });
