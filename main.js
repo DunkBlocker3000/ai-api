37ec0c4c09ba015e2df3a46bc977c011f0e54363
DunkBlocker3000 <36441422+DunkBlocker3000@users.noreply.github.com>
2023-04-22 16:59:56 +1200

    Update main.js

--- main.js
+++ main.js
@@ -1,31 +1,38 @@
-const scenariosList = document.getElementById("scenariosList");
+const inputElement = document.getElementById("input");
+const generateBtn = document.getElementById("generateBtn");
+const outputElement = document.getElementById("output");
 
-function generateScenariosFromUrl() {
-  const urlInput = document.getElementById("urlInput").value;
-  fetch(urlInput)
-    .then((response) => response.text())
-    .then((data) => generateScenarios(data));
-}
-
-function generateScenariosFromText() {
-  const apiTextArea = document.getElementById("apiTextArea").value;
-  generateScenarios(apiTextArea);
-}
-
-function generateScenarios(apiData) {
-  // Logic to parse the API data and generate scenarios
-  // ...
-
-  // Example scenarios to be added to the list
-  const scenarios = ["Scenario 1", "Scenario 2", "Scenario 3"];
-
-  // Clear the list of previous scenarios
-  scenariosList.innerHTML = "";
-
-  // Add the new scenarios to the list
-  for (const scenario of scenarios) {
-    const listItem = document.createElement("li");
-    listItem.innerText = scenario;
-    scenariosList.appendChild(listItem);
+generateBtn.addEventListener("click", async () => {
+  outputElement.innerHTML = "Generating scenarios...";
+  const input = inputElement.value.trim();
+  if (input.startsWith("http")) {
+    try {
+      const response = await fetch(input);
+      const text = await response.text();
+      const scenarios = await generateScenariosFromApiDoc(text);
+      outputElement.innerHTML = scenarios;
+    } catch (error) {
+      outputElement.innerHTML = `Error generating scenarios: ${error.message}`;
+    }
+  } else {
+    const scenarios = await generateScenariosFromApiDoc(input);
+    outputElement.innerHTML = scenarios;
   }
+});
+
+async function generateScenariosFromApiDoc(apiDoc) {
+  // replace any line breaks or tabs with spaces to help with tokenization
+  const cleanApiDoc = apiDoc.replace(/[\n\t]/g, " ");
+  
+  // generate scenarios using OpenAI's GPT-3 API
+  const completions = await openai.complete({
+    engine: "text-davinci-002",
+    prompt: `Generate test scenarios from the following API documentation:\n\n${cleanApiDoc}\n\nScenario 1:`,
+    maxTokens: 1024,
+    n: 1,
+    stop: "Scenario"
+  });
+  
+  const scenarios = completions.choices[0].text.trim();
+  return scenarios;
 }
