c4c55429cae2e1ce19dfdfedab9219059a8e957c
DunkBlocker3000 <36441422+DunkBlocker3000@users.noreply.github.com>
2023-04-22 17:37:10 +1200

    Update in.js

--- in.js
+++ in.js
@@ -1,9 +1,32 @@
-module.exports = {
-  mode: 'production',
-  target: 'web',
-  entry: './src/main.js',
-  output: {
-    path: __dirname,
-    filename: 'main.js'
-  }
+let generateScenarios = async (input) => {
+    const completions = await openai.complete({
+        engine: 'text-davinci-002',
+        prompt: `${input}\n\nExample scenarios that could happen as a result of the above text:`,
+        maxTokens: 1024,
+        n: 1,
+        stop: '\n\n',
+        temperature: 0.7,
+    });
+    const scenarioList = document.getElementById('scenario-list');
+    scenarioList.innerHTML = '';
+    const scenarios = completions.choices[0].text.trim().split('\n');
+    scenarios.forEach((scenario) => {
+        const scenarioItem = document.createElement('li');
+        scenarioItem.innerText = scenario;
+        scenarioList.appendChild(scenarioItem);
+    });
 };
+
+let handleGenerateBtnClick = async () => {
+    const inputType = document.getElementById('input-type').value;
+    const input = inputType === 'url' ? document.getElementById('url-input').value : document.getElementById('text-input').value;
+    if (!input) {
+        return;
+    }
+    await generateScenarios(input);
+};
+
+document.addEventListener('DOMContentLoaded', () => {
+    const generateBtn = document.getElementById('generate-btn');
+    generateBtn.addEventListener('click', handleGenerateBtnClick);
+});
